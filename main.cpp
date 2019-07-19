ca5b4b508f756d59f9737f30d1a765a2b114b6f2
Mohcine Chraibi <m.chraibi@gmail.com>
2019-07-19 17:47:53 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+//g++-8 main.cpp -std=c++17 -lstdc++fs
+
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include <filesystem>
+
+
+
+namespace fs = std::filesystem;
+
+
+int main()
+{
+
+    fs::path pathToShow("test.txt");
+     return 0;
+}
