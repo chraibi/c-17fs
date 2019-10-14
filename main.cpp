09a06298d4bd9496c79fd0db27a70256085dee0b
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2019-10-14 11:40:21 +0200

    add output and remove flags from cmake

--- main.cpp
+++ main.cpp
@@ -14,6 +14,14 @@ namespace fs = std::filesystem;
 int main()
 {
 
-    fs::path pathToShow("test.txt");
+      fs::path pathToShow("../main.cpp");
+      std::cout << "exists() = " << fs::exists(pathToShow) << "\n"
+           << "root_name() = " << pathToShow.root_name() << "\n"
+           << "root_path() = " << pathToShow.root_path() << "\n"
+           << "relative_path() = " << pathToShow.relative_path() << "\n"
+           << "parent_path() = " << pathToShow.parent_path() << "\n"
+           << "filename() = " << pathToShow.filename() << "\n"
+           << "stem() = " << pathToShow.stem() << "\n"
+           << "extension() = " << pathToShow.extension() << "\n";
      return 0;
 }
